c140714bea07615effa8f63cfeb3fc403f2fe775
Robert Granja <robeertgr@live.com>
2022-04-17 15:20:22 -0300

    Data types

--- script.js
+++ script.js
@@ -1,23 +1,45 @@
-let js = 'amazing';
-console.log(40 + 8 + 23 - 10);
+/*
+    let js = 'amazing';
+    console.log(40 + 8 + 23 - 10);
 
-console.log('Jonas');
-console.log(23)
+    console.log('Jonas');
+    console.log(23)
 
-let firstName = "Matilda"
-let first = 'jonas'
+    let firstName = "Matilda"
+    let first = 'jonas'
 
-console.log(firstName)
-console.log(firstName)
-console.log(firstName)
+    console.log(firstName)
+    console.log(firstName)
+    console.log(firstName)
 
-let jonas_matilda = "JM";
-let $function = 27;
+    // Variable name conventions
+    let jonas_matilda = "JM";
+    let $function = 27;
 
-let person = "jonas";
-let PI = 3.1415;
+    let person = "jonas";
+    let PI = 3.1415;
 
-let myFirstJob = 'Coder';
-let myCurrentJob = 'Teacher';
+    let myFirstJob = 'Coder';
+    let myCurrentJob = 'Teacher';
 
-console.log(myFirstJob);
+    console.log(myFirstJob);
+*/
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Jonas')
+
+javascriptIsFun = 'YES!'
+console.log(typeof javascriptIsFun)
+
+let year;
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
