cd6ff7f0bce53736c8fa7027062c87e353db9951
Robert Granja <robeertgr@live.com>
2022-04-17 17:06:56 -0300

    differences between let, const and var

--- script.js
+++ script.js
@@ -23,7 +23,7 @@
     let myCurrentJob = 'Teacher';
 
     console.log(myFirstJob);
-*/
+
 
 let javascriptIsFun = true;
 console.log(javascriptIsFun);
@@ -43,3 +43,18 @@ year = 1991;
 console.log(typeof year);
 
 console.log(typeof null);
+*/
+
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+
+// const job;
+
+var job = 'programmer';
+job = 'teacher';
+
+lastName = 'Schmedtmann';
+console.log(lastName)
