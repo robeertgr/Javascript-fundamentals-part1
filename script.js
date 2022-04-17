a13bcc6fcf1ea23474e1f7af7df2a2acbcc832c2
Robert Granja <robeertgr@live.com>
2022-04-17 17:32:40 -0300

    Basic operators

--- script.js
+++ script.js
@@ -45,16 +45,46 @@ console.log(typeof year);
 console.log(typeof null);
 */
 
-let age = 30;
-age = 31;
+// let age = 30;
+// age = 31;
 
-const birthYear = 1991;
-// birthYear = 1990;
+// const birthYear = 1991;
+// // birthYear = 1990;
 
-// const job;
+// // const job;
 
-var job = 'programmer';
-job = 'teacher';
+// var job = 'programmer';
+// job = 'teacher';
 
-lastName = 'Schmedtmann';
-console.log(lastName)
+// lastName = 'Schmedtmann';
+// console.log(lastName)
+
+// Math operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2020;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3)
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = 'Jonas';
+const lastName = 'Schmedtmann';
+console.log(firstName + ' ' + lastName);
+
+// Assignment operators
+let x = 10 + 5; // 15
+x += 10;    // x = x + 10 = 25
+x *= 4;     // x = x * 4 = 100
+x++;
+x--;
+x--;
+console.log(x);
+
+// Comparison operators
+console.log(ageJonas > ageSarah);    // >, <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2020);
