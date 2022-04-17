7f26be97a373d0b166b52607e0baae798a2070b1
Robert Granja <robeertgr@live.com>
2022-04-17 14:40:33 -0300

    First assignment

---
+++ assignments.js
@@ -0,0 +1,9 @@
+// First assignment
+
+let country = "Brazil";
+let continent = "South America";
+let population = 214488209;
+
+console.log(country);
+console.log(continent);
+console.log(population);
---
+++ script.js
@@ -0,0 +1,23 @@
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log('Jonas');
+console.log(23)
+
+let firstName = "Matilda"
+let first = 'jonas'
+
+console.log(firstName)
+console.log(firstName)
+console.log(firstName)
+
+let jonas_matilda = "JM";
+let $function = 27;
+
+let person = "jonas";
+let PI = 3.1415;
+
+let myFirstJob = 'Coder';
+let myCurrentJob = 'Teacher';
+
+console.log(myFirstJob);
