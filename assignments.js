4106abe1621e77180913452bbed845a14fe12dd4
Robert Granja <robeertgr@live.com>
2022-04-17 15:20:38 -0300

    Assignment #2

--- assignments.js
+++ assignments.js
@@ -1,4 +1,4 @@
-// First assignment
+// Assignment #1
 
 let country = "Brazil";
 let continent = "South America";
@@ -7,3 +7,13 @@ let population = 214488209;
 console.log(country);
 console.log(continent);
 console.log(population);
+
+// Assignment #2
+
+let isIsland = country;
+isIsland = true;
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
