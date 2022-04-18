06b551e9ef9c08e35d484603444d270eb56d2c5e
Robert Granja <robeertgr@live.com>
2022-04-18 00:06:58 -0300

    Strings and template literals

--- script.js
+++ script.js
@@ -43,51 +43,51 @@ year = 1991;
 console.log(typeof year);
 
 console.log(typeof null);
-*/
 
-// let age = 30;
-// age = 31;
 
-// const birthYear = 1991;
-// // birthYear = 1990;
+let age = 30;
+age = 31;
 
-// // const job;
+const birthYear = 1991;
+ birthYear = 1990;
 
-// var job = 'programmer';
-// job = 'teacher';
+ const job;
 
-// lastName = 'Schmedtmann';
-// console.log(lastName)
+var job = 'programmer';
+job = 'teacher';
 
-// Math operators
-// const now = 2037;
-// const ageJonas = now - 1991;
-// const ageSarah = now - 2020;
-// console.log(ageJonas, ageSarah);
+lastName = 'Schmedtmann';
+console.log(lastName)
 
-// console.log(ageJonas * 2, ageJonas / 10, 2 ** 3)
-// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+Math operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2020;
+console.log(ageJonas, ageSarah);
 
-// const firstName = 'Jonas';
-// const lastName = 'Schmedtmann';
-// console.log(firstName + ' ' + lastName);
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3)
+2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
 
-// Assignment operators
-// let x = 10 + 5; // 15
-// x += 10;    // x = x + 10 = 25
-// x *= 4;     // x = x * 4 = 100
-// x++;
-// x--;
-// x--;
-// console.log(x);
+const firstName = 'Jonas';
+const lastName = 'Schmedtmann';
+console.log(firstName + ' ' + lastName);
 
-// Comparison operators
-// console.log(ageJonas > ageSarah);    // >, <, >=, <=
-// console.log(ageSarah >= 18);
+Assignment operators
+let x = 10 + 5; // 15
+x += 10;    // x = x + 10 = 25
+x *= 4;     // x = x * 4 = 100
+x++;
+x--;
+x--;
+console.log(x);
 
-// const isFullAge = ageSarah >= 18;
+Comparison operators
+console.log(ageJonas > ageSarah);    // >, <, >=, <=
+console.log(ageSarah >= 18);
 
-// console.log(now - 1991 > now - 2020);
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2020);
 
 const now = 2037;
 const ageJonas = now - 1991;
@@ -101,3 +101,26 @@ console.log(x, y)
 
 const averageAge = (ageJonas + ageSarah) / 2
 console.log(ageJonas, ageSarah, averageAge);
+*/
+
+const firstName = 'Jonas';
+const job = 'teacher';
+const birthYear = 1991;
+const year = 2037;
+
+const jonas = "I'm " + firstName + ', a ' + (year - birthYear) + ' years old ' + job + '!';
+console.log(jonas);
+
+// Template strings
+const jonasNew = `I'm ${firstName}, a ${year - birthYear} years old ${job}!`;
+console.log(jonasNew);
+
+console.log(`Just a regular string...`);
+
+console.log('String with \n\
+multiple \n\
+lines');
+
+console.log(`String
+multiple
+lines`);
