001d48b9ff85cdd47a837d438a009c725e82554e
Robert Granja <robeertgr@live.com>
2022-04-17 22:10:18 -0300

    Operator precedence

--- script.js
+++ script.js
@@ -60,31 +60,44 @@ console.log(typeof null);
 // console.log(lastName)
 
 // Math operators
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2020;
+// console.log(ageJonas, ageSarah);
+
+// console.log(ageJonas * 2, ageJonas / 10, 2 ** 3)
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+// const firstName = 'Jonas';
+// const lastName = 'Schmedtmann';
+// console.log(firstName + ' ' + lastName);
+
+// Assignment operators
+// let x = 10 + 5; // 15
+// x += 10;    // x = x + 10 = 25
+// x *= 4;     // x = x * 4 = 100
+// x++;
+// x--;
+// x--;
+// console.log(x);
+
+// Comparison operators
+// console.log(ageJonas > ageSarah);    // >, <, >=, <=
+// console.log(ageSarah >= 18);
+
+// const isFullAge = ageSarah >= 18;
+
+// console.log(now - 1991 > now - 2020);
+
 const now = 2037;
 const ageJonas = now - 1991;
 const ageSarah = now - 2020;
-console.log(ageJonas, ageSarah);
-
-console.log(ageJonas * 2, ageJonas / 10, 2 ** 3)
-// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
-
-const firstName = 'Jonas';
-const lastName = 'Schmedtmann';
-console.log(firstName + ' ' + lastName);
-
-// Assignment operators
-let x = 10 + 5; // 15
-x += 10;    // x = x + 10 = 25
-x *= 4;     // x = x * 4 = 100
-x++;
-x--;
-x--;
-console.log(x);
-
-// Comparison operators
-console.log(ageJonas > ageSarah);    // >, <, >=, <=
-console.log(ageSarah >= 18);
-
-const isFullAge = ageSarah >= 18;
 
 console.log(now - 1991 > now - 2020);
+
+let x, y;
+x = y = 25 - 10 - 5;    // x = y = 10
+console.log(x, y)
+
+const averageAge = (ageJonas + ageSarah) / 2
+console.log(ageJonas, ageSarah, averageAge);
