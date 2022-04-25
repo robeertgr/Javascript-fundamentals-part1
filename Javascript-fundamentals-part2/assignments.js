2a0d477c0ce7ec1d48a3875f90a9b5a13d795146
Robert Granja <robeertgr@live.com>
2022-04-25 01:31:31 -0300

    Assignment #1

---
+++ Javascript-fundamentals-part2/assignments.js
@@ -0,0 +1,7 @@
+function describeCountry(country, population, capitalCity) {
+    return console.log(`${country} has ${population} million people and its capital city is ${capitalCity}`);
+}
+
+describeCountry('Finland', 6000000, 'Helsinki');
+describeCountry('Brazil', 230000000, 'Brazilia');
+describeCountry('Ireland', 5000000, 'Dublin');
