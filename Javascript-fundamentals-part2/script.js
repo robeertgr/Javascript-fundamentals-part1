91849e41afc04d324a5f67550304e9b6e3a01281
Robert Granja <robeertgr@live.com>
2022-04-25 11:41:48 -0300

    Arrow Functions

--- Javascript-fundamentals-part2/script.js
+++ Javascript-fundamentals-part2/script.js
@@ -38,7 +38,7 @@ const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice);
 
 const num = Number('23');
-*/
+
 
 // ** Function Declarations vs Expressions
 
@@ -56,3 +56,24 @@ const calcAge2 = function (birthYear) {
 
 const age2 = calcAge2(1991);
 console.log(age1, age2);
+*/
+
+// Function expression
+const calcAge2 = function (birthYear) {
+    return 2037 - birthYear;
+}
+
+// Arrow function
+const calcAge3 = birthYear => 2037 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2037 - birthYear;
+    const retirement = 65 - age;
+    // return retirement;
+    return `${firstName} retires in ${retirement} years`;
+}
+
+console.log(yearsUntilRetirement(1991, 'Jonas'));
+console.log(yearsUntilRetirement(1980, 'Bob'));
