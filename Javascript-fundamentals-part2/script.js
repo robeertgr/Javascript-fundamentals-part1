bf900258f945c42678b982db0c10a0ef8fd897cc
Robert Granja <robeertgr@live.com>
2022-04-28 13:13:13 -0300

    Dot vs. Bracket Notation

--- Javascript-fundamentals-part2/script.js
+++ Javascript-fundamentals-part2/script.js
@@ -158,7 +158,7 @@ console.log(age1, age2, age3);
 
 const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])];
 console.log(ages);
-*/
+
 
 const friends = ['Michael', 'Steven', 'Peter'];
 const newLength = friends.push('Jay');
@@ -195,3 +195,54 @@ console.log(friends.includes(23));
 if (friends.includes('Steven')) {
     console.log('You have a friend called Steven');
 }
+
+
+// Introduction to Objects
+
+const jonasArray = [
+    'Jonas',
+    'Schmedtmann',
+    2037 - 1991,
+    'teacher',
+    ['Michael', 'Peter', 'Steven']
+];
+
+const jonas = {
+    firstName: 'Jonas',
+    lastName: 'Schmedtmann',
+    age: 2037 - 1991,
+    job: 'teacher',
+    friends: ['Michael', 'Peter', 'Steven']
+};
+*/
+// Dot vs. Bracket Notation
+
+const jonas = {
+    firstName: 'Jonas',
+    lastName: 'Schmedtmann',
+    age: 2037 - 1991,
+    job: 'teacher',
+    friends: ['Michael', 'Peter', 'Steven']
+};
+
+console.log(jonas);
+console.log(jonas.lastName);
+console.log(jonas['lastName']); // -> You can put any expressions
+
+const nameKey = 'Name';
+console.log(jonas['first' + nameKey]);
+console.log(jonas['last' + nameKey]);
+
+const interstedIn = prompt('What do you want to know about Jonas? Choose between firstName, lastName, age, job and friends');
+console.log(jonas[interstedIn]);
+
+(jonas[interstedIn]) ? console.log(jonas[interstedIn]) : console.log('Wrong request! Choose between firstName, lastName, age, job and friends');
+
+jonas.location = 'Portugal';
+jonas['twitter'] = '@jonasschemdtmann';
+console.log(jonas);
+
+// Challenge
+// 'Jonas has 3 friends, and his best friend is called Michael'
+
+console.log(`${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`);
