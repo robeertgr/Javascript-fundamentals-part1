9badf3ceea218bf823a6926d014f07a14033bee3
Robert Granja <robeertgr@live.com>
2022-08-08 21:59:34 -0300

    Starting course

---
+++ 01-Javascript-fundamentals-part1/script.js
@@ -0,0 +1,25 @@
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log('Robert');
+console.log(23);
+
+let firstName = "Letícia";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let robert_granja = "RG";
+let $function = 27;
+
+let person = "Robert";
+let PI = 3.14159;
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Developer";
+
+let job1 = "coder";
+let job2 = "developer";
+
+console.log(myFirstJob);
